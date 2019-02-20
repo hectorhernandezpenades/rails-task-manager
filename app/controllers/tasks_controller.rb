7cf71658361cca771b7db83b9873dd590cfa2167
Hector Hernandez <hectorhernandezpenades@gmail.com>
2019-02-20 11:01:15 +0000

    displays tasks menu and task, still need to add new task form, task editor, remove a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,27 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
